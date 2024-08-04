51b4c204ba2f4c5d213e9726c6392b0837ec30da
Flavio Repos <buzonplataforma12@gmail.com>
2024-08-03 19:24:47 -0600

    Complete todos

--- client/src-sw.js
+++ client/src-sw.js
@@ -27,4 +27,30 @@ warmStrategyCache({
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
-registerRoute();
+
+// It ensures that these assets are served from the cache (stale) while
+// fetching updated versions in the background (revalidate), providing a
+// good balance between performance and freshness for these assets.
+
+// registering a route in a service worker using Workbox
+
+// The function checks if the request's destination is one of 'style',
+// 'script', or 'worker'
+// The StaleWhileRevalidate strategy is used to serve the cached response
+// (stale) while simultaneously fetching a new response (revalidate) in the
+// background.
+// CacheableResponsePlugin: A plugin used to configure which response
+// statuses should be cached. In this case, responses with status codes 0
+// (for opaque responses) and 200 (successful responses) will be cached.
+
+registerRoute(
+  ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
+  new StaleWhileRevalidate({
+    cacheName: 'asset-cache',
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+    ],
+  })
+);
--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,21 +1,54 @@
 import { openDB } from 'idb';
 
+// function to initializing the database
 const initdb = async () =>
   openDB('jate', 1, {
     upgrade(db) {
+      // Check at StoreNames if exists a database named 'jate'
       if (db.objectStoreNames.contains('jate')) {
         console.log('jate database already exists');
         return;
       }
+      // if 'jate' not exists, then creates the database
       db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
       console.log('jate database created');
     },
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
-
+// function to save data to the database
+export const putDb = async (content) => {
+  console.error('objectstore.put to save data to database');
+  // open 'jate' data base 
+  const jateDb = await openDB('jate', 1);
+  // start a 'readwrite' transaction to access the objectstore
+  const tx = jateDb.transaction('jate', 'readwrite'); 
+  const store = tx.objectStore('jate');
+  // insert (if not exist) or update a record
+  const request = store.put({ id: 1, value: content });
+  // wait for the put operation
+  const result = await request;
+  console.log('data saved', result.value);
+};
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
-
+// function to get data from the database
+export const getDb = async () => {
+  // open the database
+  const jateDb = await openDB('jate', 1);
+  // start a readonly transaction to have the objectstore available 
+  const tx = jateDb.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  // get the data from database
+  const request = store.get(1);
+  // waiting for the get request
+  const result = await request;
+  result
+    ? console.log('Data retrieved from the database', result.value)
+    : console.log('Data not found in the database');
+  // using optional chaining
+  // the optional chaining (?.) operator accesses an object's property or calls a function.
+  // If the object accessed or function called using this operator is undefined or null,
+  // the expression short circuits and evaluates to undefined instead of throwing an error.
+  return result?.value;
+};
 initdb();
